8da1eceee2dd38724ef91d5c68faf5b2df5f9793
Likinet <vivigalvagno@gmail.com>
2022-10-18 09:55:22 -0300

    Punto de inflexión

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+document.write(`
+<footer>
+    <a href="https://www.facebook.com/" target="_blank"><img src="icons/facebook.png" alt="facebook"></a>
+    <a href="https://www.instagram.com/" target="_blank"><img src="icons/instagram.png" alt="instagram"></a>
+    <a href="https://telegram.org/" target="_blank"><img src="icons/telegrama.png" alt="telegram"></a>
+    <a href="https://www.whatsapp.com/" target="_blank"><img src="icons/whatsapp.png" alt="whatsapp"></a>
+</footer>
+`)
---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.write(`
+<header>
+    <img id="logo__img" src="icons/leaf_icon.png" alt="logo">
+    <h1>Proyecto Reliquia</h1>
+</header>
+
+<nav>
+    <a class="botonNav" href="index.html">Inicio</a>
+    <a class="botonNav" href="aboutUs.html">Nosotros</a>
+    <a class="botonNav" href="greenSpots.html">Puntos verdes</a>
+    <a class="botonNav" href="contact.html">Contacto</a>
+</nav>
+`)
