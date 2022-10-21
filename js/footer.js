3eb8f89902cbc845791634db2c48fa0a7679ae68
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2022-10-21 12:28:30 -0300

    update href telegram link footer

--- js/footer.js
+++ js/footer.js
@@ -3,6 +3,6 @@ document.write(`
     <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><img class="fab facebook" src="./icons/facebook.png"></a>
     <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img class="fab instagram" src="icons/instagram.png"></a>
     <a href="https://chat.whatsapp.com/BiaPEpimmAbAiAVhz3EZm9" target="_blank" rel="noopener noreferrer"><img class="fab whatsapp" src="icons/whatsapp.png"></a>
-    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img class="fab telegram" src="icons/telegram.png"></a>
+    <a href="https://t.me/+5491134038784" target="_blank" rel="noopener noreferrer"><img class="fab telegram" src="icons/telegram.png"></a>
 </footer>
 `)
