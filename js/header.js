8c355fa97125d8aa427ce3d3edac2cc1b7dcf121
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2022-10-21 08:28:41 -0300

    ok

--- js/header.js
+++ js/header.js
@@ -1,14 +1,12 @@
 document.write(`
 <header>
-    <a href="index.html" id="logo"><img src="icons/leaf-icon.png" alt="logo">
-    PROYECTO RELIQUIA
-    </a>
+    <a href="index.html" id="logo"><img src="icons/leaf-icon.png" alt="logo">PROYECTO RELIQUIA</a>
 
     <nav>
-    <a class="botonNav" href="index.html"><b>Inicio</b></a>
-    <a class="botonNav" href="aboutUs.html"><b>Nostros</b></a>
-    <a class="botonNav" href="greenSpots.html"><b>Puntos Verdes</b></a>
-    <a class="botonNav" href="contact.html"><b>Contacto</b></a>
-</nav>
+        <a class="botonNav" href="index.html"><b>Inicio</b></a>
+        <a class="botonNav" href="aboutUs.html"><b>Nostros</b></a>
+        <a class="botonNav" href="greenSpots.html"><b>Puntos Verdes</b></a>
+        <a class="botonNav" href="contact.html"><b>Contacto</b></a>
+    </nav>
 </header>
 `)
