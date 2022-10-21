9900e31a48753900ccb6552f41652f3cc5f99200
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2022-10-21 09:24:22 -0300

    change origen de iconos footer

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,8 @@
 document.write(`
 <footer>
-    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><img class="fab facebook" src="./icons/facebook.svg"></a>
-    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img class="fab instagram" src="icons/instagram.svg"></a>
-    <a href="https://chat.whatsapp.com/BiaPEpimmAbAiAVhz3EZm9" target="_blank" rel="noopener noreferrer"><img class="fab whatsapp" src="icons/whatsapp.svg"></a>
-    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img class="fab telegram" src="icons/telegram.svg"></a>
+    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><img class="fab facebook" src="./icons/facebook.png"></a>
+    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img class="fab instagram" src="icons/instagram.png"></a>
+    <a href="https://chat.whatsapp.com/BiaPEpimmAbAiAVhz3EZm9" target="_blank" rel="noopener noreferrer"><img class="fab whatsapp" src="icons/whatsapp.png"></a>
+    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img class="fab telegram" src="icons/telegram.png"></a>
 </footer>
 `)
