abd90f82479eea62e4859d9741f1eff5da846847
Ivan Exequiel Carneiro <ivanexequielc@gmail.com>
2022-10-21 01:50:36 -0300

    ok

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.write(`
 <footer>
-    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook"></i></a>
+    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><img class="fab" #id="fa-facebook" src="images/icons/facebook.svg"></a>
     <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
     <a href="https://chat.whatsapp.com/BiaPEpimmAbAiAVhz3EZm9" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp"></i></a>
     <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i class="fab fa-telegram"></i></a>
